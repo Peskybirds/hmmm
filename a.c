e3a39c9db6d326af0a7f7d01e29e71d0f1599db5
Peskybirds <116131241+Peskybirds@users.noreply.github.com>
2024-04-11 11:53:45 +0100

    Update a.c

--- a.c
+++ a.c
@@ -1,1 +1,22 @@
-//ignore this
+#include <stdio.h>
+int main()
+{
+int T;
+int value;
+int why;
+int hlog;
+int wlog;
+if(T=2000000000){
+    why=value*why/3;
+    if(why<=1){
+        T++;
+    }
+    if(why>hlog){
+        hlog=why;
+        wlog=value;
+    }
+    T=value;
+    printf(hlog,wlog);
+}
+
+}
